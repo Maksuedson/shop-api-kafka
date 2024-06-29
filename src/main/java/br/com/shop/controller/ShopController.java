eb8e14e26557fd124ee07eeaca2acd9a9867fb9d
Maksuedson <maksuedson@hotmail.com>
2024-06-29 15:29:09 -0300

    Criado metdo Post saveShop

--- src/main/java/br/com/shop/controller/ShopController.java
+++ src/main/java/br/com/shop/controller/ShopController.java
@@ -1,13 +1,19 @@
 package br.com.shop.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.kafka.common.Uuid;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.shop.dto.ShopDTO;
+import br.com.shop.model.Shop;
+import br.com.shop.model.ShopItem;
 import br.com.shop.repository.ShopRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -27,4 +33,19 @@ public class ShopController {
 				.collect(Collectors.toList());
 	}
 	
+	@PostMapping
+	public ShopDTO saveShop(@RequestBody ShopDTO shopDTO) {
+		shopDTO.setIdentifier(Uuid.randomUuid().toString());
+		shopDTO.setDateShop(LocalDate.now());
+		shopDTO.setStatus("PENDING");
+		
+		Shop shop = Shop.convert(shopDTO);
+
+		for (ShopItem shopItem: shop.getItems()) {
+			shopItem.setShop(shop);
+		}
+		
+		return ShopDTO.convert(shopRepository.save(shop));
+
+	}
 }
