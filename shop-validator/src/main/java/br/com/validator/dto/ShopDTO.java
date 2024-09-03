ab968d321d11f2958c570763e2d2343ded16d482
Maksuedson <maksuedson@hotmail.com>
2024-09-03 18:31:27 -0300

    Criado classes dto

---
+++ shop-validator/src/main/java/br/com/validator/dto/ShopDTO.java
@@ -0,0 +1,17 @@
+package br.com.validator.dto;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ShopDTO	{
+	private	String identifier;
+	private	LocalDate dateShop;
+	private	String status;
+	private	List<ShopItemDTO> items = new ArrayList<>();	
+}
